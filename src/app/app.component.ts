1ff4269996b8b4ca3a9b08f65ebd76524146495f
Default <bancoo84@icloud.com>
2025-08-07 02:06:26 +0200

    navi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from './home/home.component';
+import { HotelsComponent } from './hotels/hotels.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { BookedroomsComponent } from './bookedrooms/bookedrooms.component';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent,HomeComponent,HotelsComponent,RoomsComponent,BookedroomsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HotelsComponent } from './hotels/hotels.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { BookedroomsComponent } from './bookedrooms/bookedrooms.component';
+import { ErrorComponent } from './error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {
+    path:'',
+    redirectTo: 'home',
+    pathMatch: 'full'
+    },
+
+    { path:'hotels', 
+      component: HotelsComponent
+    },
+
+    {path:'rooms',
+    component:RoomsComponent},
+
+    {path:'bookedrooms',
+    component:BookedroomsComponent},
+    
+    
+    {
+        path: '**',
+        component: ErrorComponent}
+    
+];
