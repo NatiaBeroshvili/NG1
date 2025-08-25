0d0ccce33b02a339c3ca335e28f66163aa4b8e1e
Default <bancoo84@icloud.com>
2025-08-25 10:23:34 +0200

    errorhandling

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,13 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getData(url : string){
-    return this.http.get(url)
+    return this.http.get(url).pipe(
+      catchError(this.errorHandling)
+    )
 
+  }
+
+  private errorHandling(err : HttpErrorResponse){
+    return throwError(()=> ("something went wrong,try later"))
   }
 }
